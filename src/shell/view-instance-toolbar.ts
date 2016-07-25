f210bc8bc0ca416ec017715ca491bb5b0562e250
Mike Graham <mgraham@technicalmedia.com>
2016-07-25 04:55:24 -0600

    rough in view-instance-toolbar buttons

--- src/shell/view-instance-toolbar.ts
+++ src/shell/view-instance-toolbar.ts
@@ -1,1 +1,8 @@
-export class ViewInstanceToolbar {}
+import { customElement, bindable } from 'aurelia-framework';
+import { ViewInstance } from '../platform/state/view-instance';
+
+@customElement('view-instance-toolbar')
+export class ViewInstanceToolbar {
+    @bindable() public activeViewInstance: ViewInstance;
+    @bindable() public viewInstances: ViewInstance[];
+}
