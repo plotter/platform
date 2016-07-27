0f471e42283927610283a688388fb4af25b89e20
Mike Graham <mgraham@technicalmedia.com>
2016-07-27 02:25:07 -0600

    x to remove tab from view instance toolbar

--- src/shell/view-instance-toolbar.ts
+++ src/shell/view-instance-toolbar.ts
@@ -6,4 +6,11 @@ export class ViewInstanceToolbar {
     @bindable() public activeViewInstance: ViewInstance;
     @bindable() public viewInstances: ViewInstance[];
     @bindable() public showTitle: boolean;
+
+    public removeItem = (vi: ViewInstance, index: number, viArr: ViewInstance[]) => {
+        viArr.splice(index, 1);
+        if (this.activeViewInstance === vi && viArr.length > 0) {
+            this.activeViewInstance = viArr[0];
+        }
+    }
 }
